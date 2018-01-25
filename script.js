17688ab2151e22062968e6bf79c660dbb4bad800
jjohanj <34948414+jjohanj@users.noreply.github.com>
2018-01-25 12:40:20 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 
-var number = ""
+var number = "";
+setInterval(testFunction, 5000);
 
 function loadDoc() {
   var x = document.getElementById("myText").value;
   var y = document.getElementById("myName").value;
   var xhttp = new XMLHttpRequest();
 
-  xhttp.open("GET", "https://www.codegorilla.nl/read_write/api.php?action=write&mykey=0101033&value=" + y +": " + x, false);
+  xhttp.open("GET", "https://www.codegorilla.nl/read_write/api.php?action=write&mykey=0101034&value=" + y +": " + x, false);
   xhttp.send();
 
   number = parseInt(xhttp.responseText);
-  var returnText = "https://www.codegorilla.nl/read_write/api.php?action=read&mykey=0101033&id=" + number;
+  var returnText = "https://www.codegorilla.nl/read_write/api.php?action=read&mykey=0101034&id=" + number;
 
   xhttp.open("GET", returnText, false);
   xhttp.send();
 
-  var newpar = document.createElement("p");
+  var newp = document.createElement("p");
 
-  var chattext = document.getElementById("demo").appendChild(newpar);
+  var chattext = document.getElementById("demo").appendChild(newp);
   chattext.innerHTML = xhttp.responseText;
   clearField();
+
+
 }
 
 function clearField() {
@@ -27,25 +30,42 @@ function clearField() {
 }
 
 function testFunction() {
+
   var xhttp = new XMLHttpRequest();
 
-  xhttp.open("GET", "https://www.codegorilla.nl/read_write/api.php?action=list&mykey=0101033", false);
+  xhttp.open("GET", "https://www.codegorilla.nl/read_write/api.php?action=list&mykey=0101034", false);
   xhttp.send();
 
   var list = xhttp.responseText;
   var b = list.split(',').map(Number);
 
+  if (number === "") {
+  number = b.slice(-1)[0];
+  console.log (number);
+  }
+  console.log (number);
+
+  var index = b.indexOf(number) + 1;
+  b.splice(0, index);
+
+
+  var xhttp = new XMLHttpRequest();
   var i;
   for (i = 0; i < b.length; i++) {
-    var xhttp = new XMLHttpRequest();
 
-    xhttp.open("GET", "https://www.codegorilla.nl/read_write/api.php?action=read&mykey=0101033&id=" + b[i], false);
+
+    xhttp.open("GET", "https://www.codegorilla.nl/read_write/api.php?action=read&mykey=0101034&id=" + b[i], false);
     xhttp.send();
 
-    var newpar2 = document.createElement("p");
-    var chattext2 = document.getElementById("demo").appendChild(newpar2);
+    var newp2 = document.createElement("p");
+    var chattext2 = document.getElementById("demo").appendChild(newp2);
     chattext2.innerHTML = xhttp.responseText;
-
+}
+console.log(number);
+console.log(b);
+if (b.length > 0){
+    var length = b.length;
+    number = number + length;
   }
 
 }
